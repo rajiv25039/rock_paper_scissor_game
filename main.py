85811f5bd8ac1c271e5d6657219f7e10836b4b89
rajiv25039 <84060115+rajiv25039@users.noreply.github.com>
2021-10-02 10:37:26 +0000

    added more logical statement

--- main.py
+++ main.py
@@ -26,12 +26,15 @@ scissors = '''
 '''
 import random
 
+game_images = [rock, paper, scissors]
 
 player_choice = int(input("What do you choose, 0 for 'rock', 1 for 'paper' and 2 for 'scissors'!\n"))
 print(f"You choose: {player_choice}")
+print(game_images[player_choice])
 
 computer_choice = random.randint(0, 2)
 print(f"Computer choose: {computer_choice}")
+print(game_images[computer_choice])
 
 if computer_choice == 0 and player_choice == 1:
   print("Congratuations. You Win!")
@@ -47,6 +50,12 @@ elif computer_choice == 1 and player_choice == 0:
 
 elif computer_choice == 1 and player_choice == 2:
   print("Congratulation, You win!")
+
+elif computer_choice == 2 and player_choice == 0: 
+  print("You lose. Try again!")
+
+elif computer_choice == 2 and player_choice == 1: 
+  print("You lose. Try again!")
 
 elif player_choice > 2 or player_choice < 0:
   print("You typed an invalid number. You lose!")
