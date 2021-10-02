46b25e2696cc2df4903e0d1fa89f4a007e016d51
rajiv25039 <84060115+rajiv25039@users.noreply.github.com>
2021-10-02 11:01:05 +0000

    debugged and code improved.

--- main.py
+++ main.py
@@ -29,34 +29,38 @@ import random
 game_images = [rock, paper, scissors]
 
 player_choice = int(input("What do you choose, 0 for 'rock', 1 for 'paper' and 2 for 'scissors'!\n"))
-print(f"You choose: {player_choice}")
-print(game_images[player_choice])
 
-computer_choice = random.randint(0, 2)
-print(f"Computer choose: {computer_choice}")
-print(game_images[computer_choice])
-
-if computer_choice == 0 and player_choice == 1:
-  print("Congratuations. You Win!")
-
-elif computer_choice == 0 and player_choice == 2:
-  print("You lose. Try again!")
-
-elif computer_choice == player_choice:
-  print("It is a draw!")
-
-elif computer_choice == 1 and player_choice == 0:
-  print("You lose. Try again!")
-
-elif computer_choice == 1 and player_choice == 2:
-  print("Congratulation, You win!")
-
-elif computer_choice == 2 and player_choice == 0: 
-  print("You lose. Try again!")
-
-elif computer_choice == 2 and player_choice == 1: 
-  print("You lose. Try again!")
-
-elif player_choice > 2 or player_choice < 0:
+if player_choice > 2 or player_choice < 0:
   print("You typed an invalid number. You lose!")
+else:
+
+  print(f"You choose: {player_choice}")
+  print(game_images[player_choice])
+
+  computer_choice = random.randint(0, 2)
+  print(f"Computer choose: {computer_choice}")
+  print(game_images[computer_choice])
+
+  if computer_choice == 0 and player_choice == 1:
+    print("Congratuations. You Win!")
+
+  elif computer_choice == 0 and player_choice == 2:
+    print("You lose. Try again!")
+
+  elif computer_choice == player_choice:
+    print("It is a draw!")
+
+  elif computer_choice == 1 and player_choice == 0:
+    print("You lose. Try again!")
+
+  elif computer_choice == 1 and player_choice == 2:
+    print("Congratulation, You win!")
+
+  elif computer_choice == 2 and player_choice == 0: 
+    print("You lose. Try again!")
+
+  elif computer_choice == 2 and player_choice == 1: 
+    print("You lose. Try again!")
+
+
 
